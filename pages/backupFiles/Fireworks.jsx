e5e4a69e0b9f45722684881b4103c36cfd6760b6
Matroid_Walker <adimensioner@gmail.com>
2023-04-22 22:02:35 +0800

    confetti stop animation

--- pages/backupFiles/Fireworks.jsx
+++ pages/backupFiles/Fireworks.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import Confetti from 'react-confetti';
+import React, { useState, useEffect } from "react";
+import Confetti from "react-confetti";
 
 const Fireworks = ({ show }) => {
   if (!show) return null;
-  return <Confetti numberOfPieces={100} />;
+  const [stopConfetti, setStopConfetti] = useState(false);
+
+  useEffect(() => {
+    // Stop the confetti after 5 seconds
+    const timeout = setTimeout(() => {
+      setStopConfetti(true);
+    }, 5000);
+
+    // Clean up the timeout
+    return () => clearTimeout(timeout);
+  }, []);
+  return (
+    <>
+      <Confetti
+        width={window.innerWidth}
+        height={window.innerHeight}
+        recycle={false}
+        numberOfPieces={200}
+        shouldStop={stopConfetti}
+      />
+    </>
+  );
 };
 
 export default Fireworks;
